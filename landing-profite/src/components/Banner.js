d4cf619a29e34bf11771e9c8388a0af9b23cd1aa
Alexander Ruiz <alexander.rivail@gmail.com>
2018-11-05 04:04:26 +0000

    faltando responsivo apenas (style)

--- landing-profite/src/components/Banner.js
+++ landing-profite/src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TituloComum from './titulo-comum';
 
+import Carousel from 'nuka-carousel';
 
 
 
@@ -8,14 +9,31 @@ class Banner extends Component {
   render() {
     return (
        <section>
-           <div className="banner">
+           <Carousel 
+            renderCenterLeftControls={({ previousSlide }) => (
+                <a onClick={previousSlide}>< img src="./assets/prev-icon.svg"/></a>
+            )}
+            renderCenterRightControls={({ nextSlide }) => (
+                <a onClick={nextSlide}>< img src="./assets/next-icon.svg"/></a>
+            )}>
+            <div className="banner">
+                <div className="titulo"><h3>Nossa especialidade: experiência de compra.</h3></div>
+                <img src="./assets/banner1.png"/>
+            </div>
+            <div className="banner">
+                <div className="titulo"><h3>Venha nos conhecer!</h3></div>
+                <img src="./assets/banner1.png"/>
+            </div>
+
+           </Carousel>
+           {/* <div className="banner">
             <ul>
                 <li>
                     <h3>Nossa especialidade: experiência de compra.</h3>
                     <img src="./assets/banner1.png"/>
                 </li>
             </ul>
-           </div>
+           </div> */}
        </section>
     );
   }
