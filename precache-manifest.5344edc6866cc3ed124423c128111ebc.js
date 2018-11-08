736f879b52ccd64b0aa541dac3d0b2a2b005fb3e
Alexander Ruiz <alexander.rivail@gmail.com>
2018-11-08 04:48:25 +0000

    Updates

---
+++ precache-manifest.5344edc6866cc3ed124423c128111ebc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c50d72c9f1a57cb7881",
+    "url": "/static/js/main.6c50d72c.chunk.js"
+  },
+  {
+    "revision": "7ca57f530f3aff01a847",
+    "url": "/static/js/1.7ca57f53.chunk.js"
+  },
+  {
+    "revision": "6c50d72c9f1a57cb7881",
+    "url": "/static/css/main.24fadfdb.chunk.css"
+  },
+  {
+    "revision": "18d11b7ec5a4e094475cbc0f479c9add",
+    "url": "/index.html"
+  }
+];
